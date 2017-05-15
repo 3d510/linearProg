eed779dd53e98009d633d1ad1d48a8b261199d3d
caluuvi <claudia.villanuevac@pucp.pe>
2017-05-15 21:25:59 +0200

    question 2 a

---
+++ Q2_b.m
@@ -0,0 +1,15 @@
+f = [1 5]*-1;
+A = [2 -1 
+     -1 1
+     1 4];
+ b = [4;
+      1;
+      12];
+ lb = [0;
+       0];
+%options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'iter');
+%options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'off');
+%options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
+%options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
+[x,fval,exitflag,output,lambda] = linprog(f, A, b, [], [], lb);
+
